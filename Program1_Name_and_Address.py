33eebd01fbacf9da882cb8ad4b2039dd550ee891
Trina J <44273053+trin8@users.noreply.github.com>
2021-03-05 16:42:00 -0800

    comments
    Remove comments

--- Program1_Name_and_Address.py
+++ Program1_Name_and_Address.py
@@ -1,20 +1,19 @@
 """
 Author: Trina Jones
-Assignment 1
 Program 1: Name and Address
 Description: This program asks the user to enter his/her name, then
 asks user to enter his/her address, then outputs the following to the console
 """
 
 # 1. read the user's Name: read first name and last name
-firstName = input("Please enter your first name: ")  # input statement, ask user to enter his/her first name
-lastName = input("Please enter your last name: ")  # input statement, ask user to enter his/her last name
-Name = (firstName + "" + lastName)  # assignment statement, store first name and last name in variable name Name
+firstName = input("Please enter your first name: ")  # input statement
+lastName = input("Please enter your last name: ")  # input statement
+Name = (firstName + "" + lastName)  # assignment statement
 
 # 2. read the user's Address: read address
-Address = input("Please enter your address: ")  # input statement, ask user to enter his/her address
+Address = input("Please enter your address: ")  # input statement
 
-# 3. Display message and user's input: message " We received the following information:", Name, Address
-print("We received the following information: ")  # output statement displaying message of info received
-print("User Name: ", Name)  # output statement displaying variable Name
-print("User address: ", Address)  # output statement displaying variable Address
+# 3. Display message and user's input
+print("We received the following information: ")  # output statement 
+print("User Name: ", Name)  # output statement
+print("User address: ", Address)  # output statement
--- Program2_Temperature_Converter.py
+++ Program2_Temperature_Converter.py
@@ -1,6 +1,5 @@
 """
 Author: Trina Jones
-Assignment 1
 Program 2: Temperature Converter
 Description: This program converts Fahrenheit to Kelvin temperatures
 the formula is as follows: K = (F + 459.67) x 5/9
@@ -9,10 +8,10 @@ and then display the temperature converted to Kelvin
 """
 
 # 1. read the user's Fahrenheit input: read fahrenheit_temperature
-fahrenheit_temperature = int(input("Please enter a temperature in Fahrenheit: "))  # input statement, store integer value from input
+fahrenheit_temperature = int(input("Please enter a temperature in Fahrenheit: "))  # input statement
 
 # 2. Convert Fahrenheit to Kelvin: kelvin (fahrenheit + 459.67 * 5/9)
-kelvin = (fahrenheit_temperature + 459.67) * 5/9  # assignment statement, create variable name Kelvin, calculate and store value
+kelvin = (fahrenheit_temperature + 459.67) * 5/9  # assignment statement
 
 # 3. Display the kelvin: print Fahrenheit temperature "degress in Fahrenheit =" Kelvin
-print( fahrenheit_temperature, "degrees in Fahrenheit    =   ", format(kelvin, '.2f'), "in Kelvin")  # output statement displaying Fahrenheit converted to Kelvin
+print( fahrenheit_temperature, "degrees in Fahrenheit    =   ", format(kelvin, '.2f'), "in Kelvin")  # output statement
